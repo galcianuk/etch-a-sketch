333debe9afbfb37cde7ec769f2824e18e7402cf0
Kyle Rowley <galcianuk@gmail.com>
2018-10-02 01:27:03 +0200

    Made the canvas responsive to browswer size.

--- script.js
+++ script.js
@@ -1,149 +1,172 @@
 
-const gridContainer = document.querySelector(".container");
+const gridContainer = document.querySelector('.container');
 
-const gridtable = document.createElement("div");
+const gridtable = document.createElement('div');
 
-const button = document.querySelector(".button");
-const clearButton = document.querySelector(".clear-button");
+const button = document.querySelector('.button');
+const clearButton = document.querySelector('.clear-button');
 
 let data = 16;
 
-button.addEventListener("click", captureData);
-clearButton.addEventListener("click", reset);
+button.addEventListener('click', captureData);
+clearButton.addEventListener('click', reset);
 
 
-//Taken from stackoverflow, I need to figure out how this works!
-Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
-}
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
+// Taken from stackoverflow, I need to figure out how this works!
+Element.prototype.remove = function () {
+  this.parentElement.removeChild(this);
+};
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+  for (let i = this.length - 1; i >= 0; i--) {
+    if (this[i] && this[i].parentElement) {
+      this[i].parentElement.removeChild(this[i]);
     }
-}
+  }
+};
 //
 
 
-function captureData(){
-    data = prompt ("What size?");
-    document.getElementsByClassName("test").remove();
+function captureData() {
+  data = prompt('What size?');
+  document.getElementsByClassName('test').remove();
 
-    generateGrid(data);
+  generateGrid(data);
 }
 
-function clearGrid(){
-    document.getElementsByClassName("test").remove();
+function clearGrid() {
+  document.getElementsByClassName('test').remove();
 
-    generateGrid(data);
+  generateGrid(data);
 }
 
-gridtable.className = "gridtable";
-gridtable.id = "gridtable";
+gridtable.className = 'gridtable';
+gridtable.id = 'gridtable';
 gridContainer.appendChild(gridtable);
 
 
 // window.onload = setWidth();
 
 let browserSize = {
-    width: window.innerWidth || document.body.clientWidth,
-    height: window.innerHeight || document.body.clientHeight
+  width: window.innerWidth || document.body.clientWidth,
+  height: window.innerHeight || document.body.clientHeight,
+};
+
+let currentBoxWidth;
+let currentBoxHeight;
+
+function setWidth() {
+    // if (browserSize.width >= 800) {
+    //    currentBoxWidth = 800;
+    // } else if (browserSize.width < 800) {
+    //   currentBoxWidth = browserSize.width;
+    // }
+    currentBoxWidth = browserSize.width - (browserSize.width / 10);;
+    //console.log(browserSize.width);
   }
 
-  let currentBoxWidth;
+  function setHeight() {
+    // if (browserSize.width >= 800) {
+    //    currentBoxWidth = 800;
+    // } else if (browserSize.width < 800) {
+    //   currentBoxWidth = browserSize.width;
+    // }
+    currentBoxHeight = browserSize.height - (browserSize.height / 20);
+  }
 
-  window.onload = generateGrid(data);
-  window.onresize = generateGrid(data);
+function generateGrid(gsize) {
+  browserSize = {
+    width: document.body.clientWidth,
+    height: document.body.clientHeight,
+  };
 
-// console.log(browserSize.width);
+  document.getElementsByClassName('test').remove();
 
+  setWidth();
+  setHeight();
 
+  // const gridSize = (gsize * gsize);
+  let boxwidth;
+  let gridSize;
+  
+  if (currentBoxWidth > currentBoxHeight) {
+    boxwidth = (currentBoxWidth / gsize);
+    boxwidth = (boxwidth) - (boxwidth / 30);
+    const gridSizeH = (currentBoxHeight / boxwidth);
+    gridSize = (gsize * gridSizeH) - 15;
+    console.log(boxwidth, gridSizeH, gridSize);
+    console.log(currentBoxHeight);
+    console.log(currentBoxWidth);
+  } else if (currentBoxWidth < currentBoxHeight) {
+    boxwidth = (currentBoxHeight / gsize);
+    boxwidth = (boxwidth) - (boxwidth / 30);
+    const gridSizeH = (currentBoxWidth / boxwidth);
+    gridSize = (gsize * gridSizeH);
+    console.log(boxwidth, gridSizeH, gridSize);
+    console.log(currentBoxHeight);
+    console.log(currentBoxWidth);
+  }
 
-
-
-function generateGrid(gsize){
-    browserSize = {
-        width: document.body.clientWidth,
-        height: document.body.clientHeight
-    }
-    
-    document.getElementsByClassName("test").remove();
-
-    setWidth();
-
-    gridSize = (gsize * gsize);
-    boxwidth = (currentBoxWidth - 2) / gsize;
-    gridtable.style = `height: ${currentBoxWidth}px; width: ${currentBoxWidth}px`;
-
-    for(i = 0; i <= (gridSize - 1); i++ ){
-        let newdiv = document.createElement("div");
-        gridtable.appendChild(newdiv);
-        newdiv.className = "test";
-        newdiv.style = `width: ${boxwidth}px; height: ${boxwidth}px`;
-    }
+  gridtable.style = `height: ${currentBoxHeight}px; width: ${currentBoxWidth}px`;
+  let i;
+  for (i = 0; i <= (gridSize - 1); i++) {
+    const newdiv = document.createElement('div');
+    gridtable.appendChild(newdiv);
+    newdiv.className = 'test';
+    newdiv.style = `width: ${boxwidth}px; height: ${boxwidth}px`;
+  }
 }
 
-function setWidth (){
-        if (browserSize.width >= 800){
-          currentBoxWidth = 800;
-        }
-        else if (browserSize.width < 800){
-          currentBoxWidth = browserSize.width;
-        }
-  }
+window.onload = generateGrid(data);
+window.onresize = generateGrid(data);
 
 let drawing = false;
-let highlightColor = "red";
+const highlightColor = 'rgb(93, 24, 121)';
 
-window.addEventListener("mousedown", function(e){
-    if (e.target.classList.contains("test")){ //On inital click, check what the div class is. If it matches the newly created "test" class - change it's color.
-        e.target.style.backgroundColor = `${highlightColor}`;    
+function highlight() {
+  const highlightdiv = document.querySelectorAll('.test');
+
+  if (drawing === true) {
+    let i;
+    for (i = 0; i < highlightdiv.length; i++) {
+      highlightdiv[i].addEventListener('mousedown', mouseDown);
+      highlightdiv[i].addEventListener('mouseover', mouseOver);
     }
-    
-    drawing = true;
-    highlight();
-})
-
-window.addEventListener("mouseup", function(e){
-    drawing = false;
-    highlight();
-})  
-
-
-function highlight(){
-    const highlightdiv = document.querySelectorAll(".test");
-
-    if (drawing == true){
-        for(i = 0; i < highlightdiv.length; i++){
-            highlightdiv[i].addEventListener("mousedown", mouseDown);
-            highlightdiv[i].addEventListener("mouseover", mouseOver);
-        }
+  } else if (drawing === false) {
+    let j;
+    for (j = 0; j < highlightdiv.length; j++) {
+      highlightdiv[j].removeEventListener('mouseover', mouseOver);
     }
-    else if (drawing == false){
-        for(j = 0; j < highlightdiv.length; j++){
-            highlightdiv[j].removeEventListener("mouseover", mouseOver);
+  }
+}
+
+window.addEventListener('mousedown', (e) => {
+  if (e.target.classList.contains('test')) { // On inital click, check what the div class is. If it matches the newly created "test" class - change it's color.
+    e.target.style.backgroundColor = `${highlightColor}`;
+  }
+  drawing = true;
+  highlight();
+});
+
+window.addEventListener('mouseup', () => {
+  drawing = false;
+  highlight();
+});
+
+  function mouseOver(e) {
+    if (drawing === true) {
+      e.target.style.backgroundColor = `${highlightColor}`;
     }
+  }
+
+
+function mouseDown(e) {
+  e.target.style.backgroundColor = `${highlightColor}`;
 }
 
-    function mouseOver(e){
-        if (drawing == true){
-                e.target.style.backgroundColor = `${highlightColor}`;
-            } else if (drawing == false){
-            };
-        }
-
+function reset() {
+  const resetdiv = document.querySelectorAll('.test');
+  let i;
+  for (i = 0; i < resetdiv.length; i++) {
+    resetdiv[i].style.backgroundColor = 'white';
+  }
 }
-
-function mouseDown(e){
-            e.target.style.backgroundColor = `${highlightColor}`;
-}
-
-function reset(){
-
-   const resetdiv = document.querySelectorAll(".test");
-   for(i = 0; i < resetdiv.length; i++){
-        resetdiv[i].style.backgroundColor = "white";
-   }
-}
-
